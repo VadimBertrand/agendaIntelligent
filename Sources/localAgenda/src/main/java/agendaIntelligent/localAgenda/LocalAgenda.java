f50475ef60638c09febb2b1d45d7f1940162c64a
VadimBertrand <vadim.bertrand@gmail.com>
2013-06-04 23:16:17 +0200

    Sources OK

---
+++ Sources/localAgenda/src/main/java/agendaIntelligent/localAgenda/LocalAgenda.java
@@ -0,0 +1,14 @@
+package agendaIntelligent.localAgenda;
+
+/**
+ * Local Agenda Interface
+ * @author <a href="http://fablab.ensimag.fr/index.php/Service_Agenda_pour_Habitat_Intelligent">Agenda Intelligent Project Team</a>
+ */
+
+public interface LocalAgenda {
+
+    /**
+     * Print a message like : "Hello $user_name"
+     */
+    void getLogin();
+}
